4aabe3624e8e6b1f191dbf0ab5b5a2dbde8bd2d0
cullen <cullengreer24@gmail.com>
2024-04-13 20:11:55 +0000

    cop third commit

---
+++ comments.js
@@ -0,0 +1,77 @@
+// Create web server
+// 1. Require express
+// 2. Create instance of express
+// 3. Create a get request for /comments
+// 4. Send back a response with a message
+// 5. Listen on port 3000
+
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+// Run the code in the terminal
+// node comments.js
+
+// Open the browser and go to http://localhost:3000/comments
+// You should see the message 'This is the comments page' on the page
+
+// Path: comments.js
+// Create web server
+// 1. Require express
+// 2. Create instance of express
+// 3. Create a get request for /comments
+// 4. Send back a response with a message
+// 5. Listen on port 3000
+
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+// Run the code in the terminal
+// node comments.js
+
+// Open the browser and go to http://localhost:3000/comments
+// You should see the message 'This is the comments page' on the page
+
+// Path: comments.js
+// Create web server
+// 1. Require express
+// 2. Create instance of express
+// 3. Create a get request for /comments
+// 4. Send back a response with a message
+// 5. Listen on port 3000
+
+const express = require('express');
+const app = express();
+
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+// Run the code in the terminal
+// node comments.js
+
+// Open the browser and go to http://localhost:3000/comments
+// You should see the message 'This is the comments page' on the page
+
+// Path: comments.js
+// Create web server
+// 1.
